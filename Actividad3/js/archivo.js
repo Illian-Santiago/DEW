031735493cd4911d5469a9b452e217043a05f741
Ildegar <98534635+Illian27@users.noreply.github.com>
2024-09-24 14:39:26 +0100

    Listo

--- Actividad3/js/archivo.js
+++ Actividad3/js/archivo.js
@@ -1,24 +1,35 @@
 let language = prompt('Select your language (English) (Russian) (Spanish)');
-let texto = document.querySelector('p');
+
+let texto = document.querySelector('h1');
+let boton = document.querySelector('button');
 
 switch (language) {
     case 'Russian':
         texto.classList.add('text-success');
+
         texto.innerHTML = 'Добро пожаловать';
+        boton.innerHTML = 'Давайте начнем';
+
         alert('Добро пожаловать');
         console.log('Добро пожаловать');
     break;
     
     case 'Spanish':
-        texto.classList.add('text-light');
+        texto.classList.add('text-dark');
+
         texto.innerHTML = 'Bienvenido';
+        boton.innerHTML = 'Comencemos';
+
         alert('Bienvenido');
         console.log('Bienvenido');
     break;
 
     case 'English':
         texto.classList.add('text-primary');
+
         texto.innerHTML = 'Welcome';
+        boton.innerHTML = 'Let´s start';
+
         alert('Welcome');
         console.log('Welcome');
         break;
