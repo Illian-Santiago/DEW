f2b6fdd59a0fcad7d2c546242cff29595800d02b
Ildegar <illianclase12345678@gmail.com>
2024-10-09 21:01:34 +0100

    cambios

--- Actividad4.Modificación_del_BOM(Window)/js/comportamientoVentanaNueva.js
+++ Actividad4.Modificación_del_BOM(Window)/js/comportamientoVentanaNueva.js
@@ -1,15 +1,24 @@
 // Mostrar el ancho y alto actual
 window.onresize = tamanoPagina;
-document.querySelector("#tamano").textContent = window.innerHeight + ' x ' + window.innerWidth;
+document.querySelector("#tamano").textContent = window.outerWidth + ' x ' + window.outerHeight;
 
 function tamanoPagina() {
-    document.querySelector("#tamano").textContent = window.innerHeight + ' x ' + window.innerWidth;
+    document.querySelector("#tamano").textContent = window.outerWidth + ' x ' + window.outerHeight;
 }
 
 // Cambiar el tamaño
-const btnTamano = querySelector('#btnTamano');
+const btnTamano = document.querySelector('#btnTamano');
+const nuevoAncho = document.querySelector('#nuevoAncho');
+const nuevoAlto = document.querySelector('#nuevoAlto');
 
+// btnTamano.addEventListener('click', cambiarTamano);
+nuevoAlto.addEventListener("mouserover", cambiarTamano)
 
 function cambiarTamano() {
-    window.innerHeight = '200';
+    let anchuraAnterior = window.outerWidth;
+    let alturaAnterior = window.outerHeight;
+
+    if (nuevoAncho.value >= 538 && nuevoAlto.value >= 190) {
+        window.resizeTo(nuevoAncho.value, nuevoAlto.value);
+    }
 }
