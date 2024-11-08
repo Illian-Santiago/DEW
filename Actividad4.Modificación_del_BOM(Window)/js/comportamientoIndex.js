98fc59074e0f958ba8c2e9be3bc45e0a16cd1501
Illian-Santiago <illianclase12345678@gmail.com>
2024-11-08 14:31:12 +0000

    CSS hecho, centrate en funcionalidad

--- Actividad4.Modificación_del_BOM(Window)/js/comportamientoIndex.js
+++
@@ -1,8 +0,0 @@
-// LLamar a una nueva página
-const btnNuevaVentana = document.querySelector('#btnNuevaVentana');
-
-btnNuevaVentana.addEventListener('click', nuevaVentana);
-
-function nuevaVentana() {
-    window.open("html/ventanaModificar.html", "Ventana para modificar", "height=401, width=600");
-}
