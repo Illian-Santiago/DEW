23d36edad99ad5385a4d4263171494bdd126cfbe
Ildegar <illianclase12345678@gmail.com>
2024-11-12 19:44:55 +0000

    Ahora funciona si el localstore

--- Actividad-4.Localstorage/script.js
+++ Actividad-4.Localstorage/script.js
@@ -3,31 +3,30 @@ const boton = document.querySelector("button");
 const contenedor = document.querySelector("div");
 const comentario = {
   id: Date.now(),
-  Autor: "Tu",
-  texto: mensaje,
+  autor: "Tu: ",
+  texto: "",
 };
 
 boton.addEventListener("click", guardarMensaje);
 
 function guardarMensaje() {
   if (mensaje.value) {
+    comentario.texto = mensaje.value;
     localStorage.setItem(localStorage.length, JSON.stringify(comentario));
+
   }
 }
 
 function mostrarMensajes() {
   for (let index = 0; index < localStorage.length; index++) {
     const comentarioActual = JSON.parse(localStorage.getItem(index));
-    console.log(comentarioActual.id);
+    const parrafo = document.createElement("p");
 
-    // const parrafo = document.createElement("p");
+    parrafo.innerHTML = `<strong>` + comentarioActual.autor + `</strong>` + comentarioActual.texto;
 
-    // parrafo.innerHTML = `<strong>` + +`</strong> Hola mundo`;
-
-    // contenedor.appendChild(parrafo);
+    contenedor.appendChild(parrafo);
   }
 }
-
 
 // setTimeout(() => {
 //   document.getElementById("alert").style.display = "none";
