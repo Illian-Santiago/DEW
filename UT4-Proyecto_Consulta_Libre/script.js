1eb26658cd0d3eac9e80238c585e82ed4600404e
Ildegar <illianclase12345678@gmail.com>
2024-11-19 21:04:47 +0000

    sdffsdasdfasdfadsfadsfdsf

--- UT4-Proyecto_Consulta_Libre/script.js
+++ UT4-Proyecto_Consulta_Libre/script.js
@@ -1,12 +1,20 @@
-const enlace = 'https://narutodb.xyz/api/clan';
+const consultarApi = document.querySelector("#consultarApi");
+const termino = document.querySelector("textarea");
+const div = document.querySelector("#apiData");
+let datos;
 
-fetch(enlace)
-    .then(data => {
-        if (!data.ok) {
-            throw Error(data.status);
-        }
+consultarApi.addEventListener('click', mostrarDatos);
 
-        return data.json();
-    })
-    .then(update => { console.log(update) })
-    .catch(error => { console.log(error) })
+function mostrarDatos() {
+    const enlace = 'https://narutodb.xyz/api/character';
+
+    fetch(enlace)
+        .then(data => { if (!data.ok) { throw Error(data.status); } return data.json(); })
+        .then(clanes => {
+            for (let index = 0; index < clanes.length; index++) {
+                const elemento = JSON.stringify(clanes[index]);
+                console.log(elemento);
+            }
+        })
+        .catch(error => { console.log(error) });
+}
