39ceb5397b25636f27c7fed4f76e5500fee937b2
Illian-Santiago <illianclase12345678@gmail.com>
2024-11-21 02:40:28 +0000

    consulta mejorada

--- UT4-Proyecto_Consulta_Libre/script.js
+++ UT4-Proyecto_Consulta_Libre/script.js
@@ -1,12 +1,12 @@
 const consultarApi = document.querySelector("#consultarApi");
-const terminoBusqueda = document.querySelector("select");
+const terminoBusqueda = document.querySelector("select").value;
 const div = document.querySelector("#apiData");
-const terminoElementos = terminoBusqueda.value + 's';
+const terminoElementos = terminoBusqueda + 's';
 
 consultarApi.addEventListener('click', verificarContenido);
 
 function cargarDatos() {
-    const enlace = 'https://narutodb.xyz/api/' + terminoBusqueda.value;
+    const enlace = 'https://narutodb.xyz/api/' + terminoBusqueda;
 
     fetch(enlace)
         .then(consulta => { // Lo que devuelva la consulta a la API
@@ -17,13 +17,29 @@ function cargarDatos() {
                 console.log(consulta.status);
             }
         })
-        .then(elementos => sessionStorage.setItem(terminoElementos, JSON.stringify(elementos)));
+        .then(elementos => {
+
+            sessionStorage.setItem(terminoElementos, JSON.stringify(elementos.terminoElementos.replace('-', '')));
+            mostrarContenido(elementos.kekkeigenkai);
+        });
 }
 
 function verificarContenido() {
     if (sessionStorage.getItem(terminoElementos) == null) {
         cargarDatos()
+    } else {
+        console.log(JSON.parse(sessionStorage.getItem(terminoElementos)));
+        mostrarContenido(JSON.parse(sessionStorage.getItem(terminoElementos))[1].characters)
     }
 }
 
-console.log(JSON.parse(sessionStorage.getItem(terminoElementos)));
+function mostrarContenido(dato, contenedor) {
+    contenedor = div;
+    for (let index = 0; index < dato.length; index++) {
+        const termino = dato[index].name;
+        const nuevoParrafo = document.createElement('p');
+        mostrarContenido
+        nuevoParrafo.textContent = JSON.stringify(termino);
+        contenedor.appendChild(nuevoParrafo);
+    }
+}
