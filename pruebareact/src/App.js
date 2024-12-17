41440628ddac5ae60b992eb5c3cc5cbec9f6edfb
Illian-Santiago <illianclase12345678@gmail.com>
2024-12-17 20:40:56 +0000

    Contador hecho

--- pruebareact/src/App.js
+++ pruebareact/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Counters from './components/counters';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hola mundo</p>
+
+        <Counters />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
